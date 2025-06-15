ddfc527b886d650618e8ff4effe740b6d4e5c2a7
fawwww <fawezteka2@gmail.com>
2025-06-15 14:22:31 +0200

    test Rebase from User 2

---
+++ whatsappclone/src/main/java/com/example/whatsappclone/common/TestRebase2.java
@@ -0,0 +1,17 @@
+package com.example.whatsappclone.common;
+
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@MappedSuperclass
+public class TestRebase2 {
+}
